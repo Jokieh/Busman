69960bca33bc436a0f9e0806bbafb1fee287dc61
Jokieh <1stone.seres@gmail.com>
2022-05-12 22:45:50 +0200

    initial commit

---
+++ src/main/java/com/jokieh/demo/BusmanApplication.java
@@ -0,0 +1,13 @@
+package com.jokieh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusmanApplication.class, args);
+	}
+
+}
