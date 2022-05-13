4e39103402c81fda9b19b18721d135ddd89ba052
Jokieh <1stone.seres@gmail.com>
2022-05-13 16:32:43 +0200

    modding gitignore

--- src/main/java/com/jokieh/demo/BusmanApplication.java
+++ src/main/java/com/jokieh/demo/BusmanApplication.java
@@ -1,7 +1,13 @@
 package com.jokieh.demo;
 
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class BusmanApplication {
